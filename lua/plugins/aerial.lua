40da4fcc6424bbd870058d00acd7cc7822c429f1
Richard Vock <gerritadmin@dminds.de>
2025-02-05 16:45:44 +0100

    aerial

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/aerial.nvim",
+  opts = {
+    -- Aerial's own setup options
+    layout = {
+      -- You can specify an integer or a float (relative to the editor width)
+      width = 30,
+      min_width = 30,
+      max_width = 60,
+      -- Choose where to place the window, etc.
+      default_direction = "prefer_right",
+    },
+    -- Other Aerial settings...
+  },
+}
