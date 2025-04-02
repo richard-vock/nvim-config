6b2bc9a07574eac869cd873140fb77183a79d75f
Richard Vock <richard@dminds.de>
2025-04-02 10:40:16 +0200

    formatting opts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local opt = vim.opt
+opt.tabstop = 4
+opt.softtabstop = 0
+opt.shiftwidth = 4
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
 vim.opt.clipboard = "unnamedplus"
+vim.opt.ignorecase = false
+vim.opt.smartcase = false
